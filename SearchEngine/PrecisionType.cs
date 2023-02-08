0ed9c06deb517eb7db0418adc5c37825c52e1eba
Артемий Соболев <titeha@yandex.ru>
2023-02-08 16:13:00 +0700

    Добавил перечисление типа точности

---
+++ SearchEngine/PrecisionType.cs
@@ -0,0 +1,14 @@
+﻿namespace SearchEngine;
+
+public enum PrecisionType
+{
+  /// <summary>
+  /// Точное количество опечаток
+  /// </summary>
+  PrecisionTypeCount,
+
+  /// <summary>
+  /// Количество опечаток в процентом отношении к длине слова
+  /// </summary>
+  PrecisionPercent
+}
