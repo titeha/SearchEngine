312e443764a8cc967abf1bc37aa208f45609b3f6
Артемий Соболев <titeha@yandex.ru>
2023-02-08 16:15:36 +0700

    Добавлено перечисление место поиска

---
+++ SearchEngine/SearchLocation.cs
@@ -0,0 +1,17 @@
+﻿namespace SearchEngine;
+
+/// <summary>
+/// Место проведения поиска
+/// </summary>
+public enum SearchLocation
+{
+  /// <summary>
+  /// С начала слова
+  /// </summary>
+  BeginWord,
+
+  /// <summary>
+  /// В слове (в том числе в начале слова)
+  /// </summary>
+  InWord
+}
