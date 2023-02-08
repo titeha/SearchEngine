3982fb89f8940621b37876a3a40553338a0390dd
Артемий Соболев <titeha@yandex.ru>
2023-02-08 16:19:38 +0700

    Добавлено перечисление типа поиска

---
+++ SearchEngine/SearchType.cs
@@ -0,0 +1,17 @@
+﻿namespace SearchEngine;
+
+/// <summary>
+/// Тип поиска
+/// </summary>
+public enum SearchType
+{
+  /// <summary>
+  /// Точный поиск
+  /// </summary>
+  ExactSearch,
+
+  /// <summary>
+  /// Неточный поиск
+  /// </summary>
+  NearSearch
+}
