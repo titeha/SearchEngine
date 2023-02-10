475c6fd832d2739707f00229240aac5995b3f9c3
Артемий Соболев <titeha@yandex.ru>
2023-02-10 11:22:49 +0700

    Добавлены методы в StringExtender. Переименован класс фонетического поиска. Добавлена заготовка класса поиска

---
+++ SearchEngine/Search.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchEngine;
+
+public partial class Search<T> where T: struct
+{
+
+}
--- SearchEngine/StringExtender.cs
+++ SearchEngine/StringExtender.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Text;
+using System.Text.RegularExpressions;
 
 namespace SearchEngine;
 
@@ -16,6 +17,31 @@ internal static partial class StringExtender
         ? _hasLatin ? CharSet.CombineCharSet : CharSet.CyrillicCharSet
         : _hasLatin ? CharSet.LatinaCharSet : CharSet.OtherCharSet;
   }
+
+  internal static string CorrectingString(string source)
+  {
+    StringBuilder _string = new(source);
+    int i = 0;
+
+    while (i < _string.Length)
+      if (" ({[<.,)}]>;:!?".IsDelimiter(_string[i]))
+        if (" .,)}]>!?:;".IsDelimiter(_string[i]) && 0 == i)
+          _string.Remove(0, 1);
+        else if (" .,:;!?".IsDelimiter(_string[i]) && "([{<".IsDelimiter(_string[i - 1]))
+          _string.Remove(i, 1);
+        else if (":;)]}>.,".IsDelimiter(_string[i]) && _string[i - 1] == ' ')
+          _string.Remove(i - 1, 1);
+        else if (i > 0 && _string[i] == _string[i - 1])
+          _string.Remove(i - 1, 1);
+        else
+          i++;
+      else
+        i++;
+
+    return _string.ToString();
+  }
+
+  internal static bool IsDelimiter(this string delimiters, char delimiter) => delimiters.IndexOf(delimiter) >= 0;
 
   [GeneratedRegex("\\p{IsCyrillic}", RegexOptions.Compiled)]
   private static partial Regex IsCyrillicRegex();
