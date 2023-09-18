87db81e0875cbee8a48cb004d16a4eb340976d1b
Артемий Соболев <sobolevas1@culture.mos.ru>
2023-09-18 16:33:50 +0700

    Добавлен метод Contains, конструктор с параметром "Только для чтения", свойство "Только для чтения"

--- SearchEngine/IndexList.cs
+++ SearchEngine/IndexList.cs
@@ -4,34 +4,42 @@
 /// Список найденных индексов
 /// </summary>
 /// <typeparam name="T">Целый числовой тип</typeparam>
-internal class IndexList<T> where T : struct
+public class IndexList<T> where T : struct
 {
-  #region Поле
+  #region Поля
   private protected readonly List<T> _indexes;
   #endregion
 
   #region Свойства
   internal int Count => _indexes.Count;
+
+  internal bool IsReadOnly { get; }
   #endregion
 
   #region Конструкторы
-  public IndexList() => _indexes = new List<T>(4);
+  public IndexList() => _indexes = new(4);
 
   public IndexList(T newIndex) : this() => _indexes.Add(newIndex);
 
   private IndexList(IEnumerable<T> indexes)
   {
-    _indexes = new List<T>(indexes);
+    _indexes = new(indexes);
     _indexes.Sort();
   }
+
+  internal IndexList(bool isReadOnly) : this() => IsReadOnly = isReadOnly;
   #endregion
 
   #region Методы
   public override string ToString() => string.Join(',', _indexes);
 
+  /// <summary>
+  /// Добавление нового элемента в список индексов
+  /// </summary>
+  /// <param name="newIndex">Добавляемый индекс</param>
   public void TryAddValue(T newIndex)
   {
-    if (!_indexes.Contains(newIndex))
+    if (!(IsReadOnly || _indexes.Contains(newIndex)))
     {
       _indexes.Add(newIndex);
       _indexes.Sort();
@@ -39,5 +47,7 @@ internal class IndexList<T> where T : struct
   }
 
   public IndexList<T> UnionIndexes(IndexList<T> otherIndexes) => new(_indexes.Union(otherIndexes._indexes));
+
+  internal bool Contains(IndexList<T> lookUpSet) => _indexes.Intersect(lookUpSet._indexes) is not null;
   #endregion
 }
