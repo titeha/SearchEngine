6a40b51c4492a90d52b3161fbcad763fcb3a0395
Артемий Соболев <sobolevas1@culture.mos.ru>
2023-09-19 14:24:20 +0700

    Исправление в расширении типа SortedList

--- SearchEngine/SearchExtender.cs
+++ SearchEngine/SearchExtender.cs
@@ -2,22 +2,7 @@
 
 internal static class SearchExtender
 {
-  public static SortedList<T, V> Concat<T, V, Z>(this SortedList<T, V> source, SortedList<T, V> added)
-  where T : struct
-  where V : IndexList<Z>
-  where Z : struct
-  {
-    SortedList<T, V> result = new(source);
-    foreach (var item in added)
-      if (result.ContainsKey(item.Key))
-        result[item.Key] = result[item.Key].UnionIndexes(item.Value) as V;
-      else
-        result[item.Key] = item.Value;
-
-    return result;
-  }
-
-  public static SortedList<T, V> Concat<T, V>(this SortedList<T, V> source, SortedList<T, V> added)
+  public static SortedList<T, V> Concat<T, V>(this SortedList<T, V> source, SortedList<T, V> added) where T : notnull
   {
     SortedList<T, V> result = new(source);
 
