dbe8d119b855c5f110483c147f87942ed33f4439
Артемий Соболев <sobolevas1@culture.mos.ru>
2023-09-22 10:10:46 +0700

    Добавил тесты для объединения индексов

--- SearchEngine.Tests/IndexListTests.cs
+++ SearchEngine.Tests/IndexListTests.cs
@@ -5,72 +5,135 @@ public class IndexListTests
   [Fact]
   public void Create_new_IndexList_Result_is_not_empty()
   {
-    IndexList<int> _testIndexList = new(1);
-    const int _expectedCountElements = 1;
+    IndexList<int> testIndexList = new(1);
+    const int expectedCountElements = 1;
 
-    Assert.Equal(_expectedCountElements, _testIndexList.Count);
+    Assert.Equal(expectedCountElements, testIndexList.Count);
   }
 
   [Fact]
   public void Add_new_elements_in_IndexList_Collection_has_new_elements_and_sorted()
   {
-    IndexList<int> _testIndexList = new();
+    IndexList<int> testIndexList = new();
 
-    _testIndexList.TryAddValue(2);
-    _testIndexList.TryAddValue(1);
-    _testIndexList.TryAddValue(4);
-    _testIndexList.TryAddValue(1);
-    _testIndexList.TryAddValue(9);
-    _testIndexList.TryAddValue(5);
+    testIndexList.TryAddValue(2);
+    testIndexList.TryAddValue(1);
+    testIndexList.TryAddValue(4);
+    testIndexList.TryAddValue(1);
+    testIndexList.TryAddValue(9);
+    testIndexList.TryAddValue(5);
 
-    const int _expectedCountElements = 5;
-    const string _expectedStringResult = "1,2,4,5,9";
+    const int expectedCountElements = 5;
+    const string expectedStringResult = "1,2,4,5,9";
 
-    Assert.Equal(_expectedCountElements, _testIndexList.Count);
-    Assert.Equal(_expectedStringResult, _testIndexList.ToString());
+    Assert.Equal(expectedCountElements, testIndexList.Count);
+    Assert.Equal(expectedStringResult, testIndexList.ToString());
   }
 
   [Fact]
   public void Union_two_lists_Returns_new_list()
   {
-    IndexList<int> _testIndexList = new();
-    IndexList<int> _otherIndexList = new();
+    IndexList<int> testIndexList = new();
+    IndexList<int> otherIndexList = new();
 
-    _testIndexList.TryAddValue(2);
-    _testIndexList.TryAddValue(1);
-    _testIndexList.TryAddValue(4);
-    _testIndexList.TryAddValue(1);
-    _testIndexList.TryAddValue(9);
-    _testIndexList.TryAddValue(5);
+    testIndexList.TryAddValue(2);
+    testIndexList.TryAddValue(1);
+    testIndexList.TryAddValue(4);
+    testIndexList.TryAddValue(1);
+    testIndexList.TryAddValue(9);
+    testIndexList.TryAddValue(5);
 
-    _otherIndexList.TryAddValue(4);
-    _otherIndexList.TryAddValue(7);
-    _otherIndexList.TryAddValue(1);
-    _otherIndexList.TryAddValue(8);
-    _otherIndexList.TryAddValue(2);
+    otherIndexList.TryAddValue(4);
+    otherIndexList.TryAddValue(7);
+    otherIndexList.TryAddValue(1);
+    otherIndexList.TryAddValue(8);
+    otherIndexList.TryAddValue(2);
 
-    IndexList<int> _resultIndexList = _testIndexList.UnionIndexes(_otherIndexList);
-    const string _expectedResultString = "1,2,4,5,7,8,9";
-    const int _expectedElementCount = 7;
+    IndexList<int> resultIndexList = testIndexList.UnionIndexes(otherIndexList);
+    const string expectedResultString = "1,2,4,5,7,8,9";
+    const int expectedElementCount = 7;
 
-    Assert.Equal(_expectedElementCount, _resultIndexList.Count);
-    Assert.Equal(_expectedResultString, _resultIndexList.ToString());
+    Assert.Equal(expectedElementCount, resultIndexList.Count);
+    Assert.Equal(expectedResultString, resultIndexList.ToString());
   }
 
   [Fact]
   public void Union_empty_list_with_list_contains_elements_Returns_list_like_senond_list()
   {
-    IndexList<int> _testIndexList = new();
-    IndexList<int> _emptyIndexList = new();
+    IndexList<int> testIndexList = new();
+    IndexList<int> emptyIndexList = new();
 
-    _testIndexList.TryAddValue(7);
-    _testIndexList.TryAddValue(2);
-    _testIndexList.TryAddValue(14);
-    const string _expectedResultString = "2,7,14";
-    const int _expectedElementCount = 3;
-    IndexList<int> _resultIndexList = _testIndexList.UnionIndexes(_emptyIndexList);
+    testIndexList.TryAddValue(7);
+    testIndexList.TryAddValue(2);
+    testIndexList.TryAddValue(14);
+    const string expectedResultString = "2,7,14";
+    const int expectedElementCount = 3;
+    IndexList<int> resultIndexList = testIndexList.UnionIndexes(emptyIndexList);
 
-    Assert.Equal(_expectedElementCount, _resultIndexList.Count);
-    Assert.Equal(_expectedResultString, _resultIndexList.ToString());
+    Assert.Equal(expectedElementCount, resultIndexList.Count);
+    Assert.Equal(expectedResultString, resultIndexList.ToString());
+  }
+
+  [Fact]
+  public void Union_list_with_collection_Result_has_all_indexes()
+  {
+    IndexList<int> testIndexList = new();
+    List<IndexList<int>> addedIndexes = new()
+    {
+      new IndexList<int>(),
+      new IndexList<int>()
+    };
+
+    testIndexList.TryAddValue(1);
+    testIndexList.TryAddValue(3);
+    testIndexList.TryAddValue(5);
+    testIndexList.TryAddValue(7);
+    addedIndexes[0].TryAddValue(2);
+    addedIndexes[0].TryAddValue(4);
+    addedIndexes[0].TryAddValue(5);
+    addedIndexes[0].TryAddValue(9);
+    addedIndexes[1].TryAddValue(7);
+    addedIndexes[1].TryAddValue(15);
+    addedIndexes[1].TryAddValue(27);
+    addedIndexes[1].TryAddValue(115);
+    const int expectedElementCount = 10;
+    const string expectedResultString = "1,2,3,4,5,7,9,15,27,115";
+
+    IndexList<int> resultIndexes = testIndexList.UnionIndexes(addedIndexes);
+
+    Assert.Equal(expectedElementCount, resultIndexes.Count);
+    Assert.Equal(expectedResultString, resultIndexes.ToString());
+  }
+
+  [Fact]
+  public void IndexList_lookup_value_using_Contains_method_with_present_value_Return_true()
+  {
+    IndexList<int> testIndexList = new();
+    IndexList<int> lookUpIndexList = new();
+
+    testIndexList.TryAddValue(1);
+    testIndexList.TryAddValue(2);
+    testIndexList.TryAddValue(10);
+    lookUpIndexList.TryAddValue(2);
+
+    var result = testIndexList.Contains(lookUpIndexList);
+
+    Assert.True(result);
+  }
+
+  [Fact]
+  public void IndexList_lookup_value_using_Contains_method_without_present_value_Return_false()
+  {
+    IndexList<int> testIndexList = new();
+    IndexList<int> lookUpIndexList = new();
+
+    testIndexList.TryAddValue(1);
+    testIndexList.TryAddValue(2);
+    testIndexList.TryAddValue(10);
+    lookUpIndexList.TryAddValue(7);
+
+    var result = testIndexList.Contains(lookUpIndexList);
+
+    Assert.False(result);
   }
 }
