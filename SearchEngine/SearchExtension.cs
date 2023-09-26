155815cf6d4d58608a3d8b3d609b7d8409a31e4a
Артемий Соболев <sobolevas1@culture.mos.ru>
2023-09-26 11:35:04 +0700

    Добавил класс расширение с методами запуска формирования словаря индексов для поиска

---
+++ SearchEngine/SearchExtension.cs
@@ -0,0 +1,37 @@
+﻿using CommonClasses;
+
+namespace SearchEngine;
+
+public static class SearchExtension
+{
+  public static void PrepareIndex<T>(this Search<T> search, IEnumerable<ISourceData<T>> source, string? delimiters = null) where T : struct
+  {
+    if (search == null)
+      NullExceptionThrow(search);
+
+    search!.PrepareIndex();
+
+    if (source?.Any() != true)
+      return;
+
+    new Search<T>.IndexBuilder(search, delimiters).BuildIndex(source);
+  }
+
+  public static void PrepareIndex<T>(this Search<T> search, string[] source, string elementDelimiter, string? delimiters = null) where T : struct
+  {
+    if (search == null)
+      NullExceptionThrow(search);
+
+    search!.PrepareIndex();
+
+    if (source?.Any() != true)
+      return;
+
+    if (elementDelimiter.IsNullOrEmpty())
+      return;
+
+    new Search<T>.IndexBuilder(search, delimiters).BuildIndex(source, elementDelimiter);
+  }
+
+  private static void NullExceptionThrow<T>(Search<T>? search) where T : struct => throw new ArgumentNullException(nameof(search));
+}
