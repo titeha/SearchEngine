27cda907ffaea233a9fad6da437bf38f06a7126b
Артемий Соболев <sobolevas1@culture.mos.ru>
2023-09-28 12:38:32 +0700

    Исправил метод IsHasIndexes на свойство

--- SearchEngine/SearchResultList.cs
+++ SearchEngine/SearchResultList.cs
@@ -10,6 +10,8 @@ public class SearchResultList<T> where T : struct
   public IndexList<T> this[int range] => !Items.ContainsKey(range) ? _emptyList : Items[range];
 
   public SortedList<int, IndexList<T>> Items { get; set; }
+
+  public bool IsHasIndex => Items.Any(i => i.Value.Count > 0);
   #endregion
 
   #region Конструктор
@@ -17,8 +19,6 @@ public class SearchResultList<T> where T : struct
   #endregion
 
   #region Методы
-  internal bool IsHasIndex(IndexList<T> lookUpIndex) => Items.Any(i => i.Value.Contains(lookUpIndex));
-
   internal void Union(SearchResultList<T> secondList)
   {
     foreach (var item in secondList.Items.Where(value => !Items.TryAdd(value.Key, value.Value)))
