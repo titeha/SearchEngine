4519f8af6ca5873e99dbdc522548df2a04f11f20
Artemy Sobolev <titeha@yandex.ru>
2026-05-06 13:45:12 +0700

    Подключить SearchEngine к сервису через ProjectReference

--- src/SearchEngine.Service/Program.cs
+++ src/SearchEngine.Service/Program.cs
@@ -1,18 +1,19 @@
-namespace SearchEngine.Service;
+using SearchEngine;
 
-public class Program
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+WebApplication app = builder.Build();
+
+app.MapGet("/health", () => Results.Ok(new
 {
-  public static void Main(string[] args)
-  {
-    WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+  Status = "ok"
+}));
 
-    WebApplication app = builder.Build();
+app.MapGet("/v1/info", () => Results.Ok(new
+{
+  Service = "TiSoft.SearchEngine.Service",
+  Status = "ok",
+  SearchEngineVersion = typeof(Search<int>).Assembly.GetName().Version?.ToString()
+}));
 
-    app.MapGet("/health", () => Results.Ok(new
-    {
-      Status = "ok"
-    }));
-
-    app.Run();
-  }
-}
+app.Run();
